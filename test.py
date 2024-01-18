f231da9a2135bb0ac5496bfb50a1ee3a209ec03c
Theviki20110 <vincenzolapadula@gmail.com>
2024-01-18 16:02:16 +0100

    up

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from web3 import Web3
 
 # Connetti a un nodo Ethereum (ad esempio, Infura)
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
-
+w3 = Web3(Web3.HTTPProvider('http://172.29.58.184:7545'))
+print(w3.is_connected())
 # Verifica la connessione
-if w3.isConnected():
+if w3.is_connected():
     print("Connesso alla blockchain Ethereum.")
