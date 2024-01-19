5593f251a05c2d870313f2dfff4e8341d62a6e44
Theviki20110 <vincenzolapadula23@gmail.com>
2024-01-19 17:32:32 +0100

    First Attempt

---
+++ sc_deploy/contracts/First_Try.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+uint256 constant TOTAL_TICKETS = 10;
+
+contract First_Try {
+  address public owner = msg.sender;
+
+  struct Ticket {
+    address owner;
+    uint256 price;
+  }
+
+  Ticket[TOTAL_TICKETS] public tickets;
+
+  constructor() {
+    for (uint256 i = 0; i < TOTAL_TICKETS; i++) {
+      tickets[i].owner = address(0x0);
+      tickets[i].price = 1e17;
+    }
+  }
+  
+  function buyTicket(uint256 ticketId) external payable {
+    require(ticketId >= 0 && ticketId < TOTAL_TICKETS, "Ticket ID does not exist");
+    require(msg.value == tickets[ticketId].price, "Not enough Ether provided");
+    require(tickets[ticketId].owner == address(0x0), "Ticket already sold");
+
+    tickets[ticketId].owner = msg.sender;
+  }
+
+}
+
+
--- test.py
+++
@@ -1,8 +0,0 @@
-from web3 import Web3
-
-# Connetti a un nodo Ethereum (ad esempio, Infura)
-w3 = Web3(Web3.HTTPProvider('http://172.29.58.184:7545'))
-print(w3.is_connected())
-# Verifica la connessione
-if w3.is_connected():
-    print("Connesso alla blockchain Ethereum.")
